527ba42cea18866db63fbfe65ad2192ee9941821
AdamPI314 <shirong.bai@colorado.edu>
2017-07-28 21:52:34 -0600

    Create /tmp.

--- src/main/java/run_SentimentAnalysis.sh
+++ src/main/java/run_SentimentAnalysis.sh
@@ -6,7 +6,17 @@ hadoop com.sun.tools.javac.Main SentimentAnalysis.java 1>stdout.txt 2>stderr.txt
 rm -f SentimentAnalysis.jar 1>/dev/null 2>&1
 jar cf SentimentAnalysis.jar *.class 1>>stdout.txt 2>>stderr.txt
 
+# create /tmp directory
+if $(hadoop fs -test -d /tmp) ;
+then
+  echo "/tmp found, delete!"; 
+  hdfs dfs -rm -r /tmp > /dev/null 2>&1
+else 
+  echo "/tmp directory not found."; 
+fi
+hdfs dfs -mkdir /tmp > /dev/null 2>&1
 
+# put emotionCategory.txt under /tmp
 if $(hadoop fs -test -f /tmp/emotionCategory.txt) ;
 then
   echo "/tmp/emotionCategory.txt found, delete!"; 
@@ -16,6 +26,7 @@ else
 fi
 hdfs dfs -put ~/src/Sentiment-Analysis/src/main/resources/emotionCategory.txt /tmp > /dev/null 2>&1
 
+# put /input
 if $(hadoop fs -test -d /input) ;
 then
   echo "/input found, delete!"; 
@@ -25,6 +36,7 @@ else
 fi
 hdfs dfs -put ~/src/Sentiment-Analysis/src/main/resources/input / > /dev/null 2>&1
 
+# check /output
 if $(hadoop fs -test -d /output) ;
 then
   echo "/output found, delete!"; 
