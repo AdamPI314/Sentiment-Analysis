56cdfdfedff007e5fc8ada65a60300f61c9e0d1c
AdamPI314 <shirong.bai@colorado.edu>
2017-07-28 20:13:11 -0600

    Update run_SentimentAnalysis.sh

--- src/main/java/run_SentimentAnalysis.sh
+++ src/main/java/run_SentimentAnalysis.sh
@@ -1,8 +1,29 @@
 #!/bin/sh
 
-hadoop com.sun.tools.javac.Main SentimentAnalysis.java
+rm *.class 1>/dev/null 2>&1
+hadoop com.sun.tools.javac.Main SentimentAnalysis.java 1>stdout.txt 2>stderr.txt
 
-jar cf SentimentAnalysis.jar *.class
+rm SentimentAnalysis.jar 1>/dev/null 2>&1
+jar cf SentimentAnalysis.jar *.class 1>>stdout.txt 2>>stderr.txt
+
+
+if $(hadoop fs -test -f /tmp/emotionCategory.txt) ;
+then
+  echo "/tmp/emotionCategory.txt found, delete!"; 
+  hdfs dfs -rm /tmp/emotionCategory.txt > /dev/null 2>&1
+else 
+  echo "/tmp/emotionCategory.txt directory not found."; 
+  hdfs dfs -put ~/src/Sentiment-Analysis/src/main/resources/emotionCategory.txt /tmp > /dev/null 2>&1
+fi
+
+if $(hadoop fs -test -d /input) ;
+then
+  echo "/input found, delete!"; 
+  hdfs dfs -rm -r /input > /dev/null 2>&1
+else 
+  hdfs dfs -put ~/src/Sentiment-Analysis/src/main/resources/input / > /dev/null 2>&1
+  echo "/input directory not found."; 
+fi
 
 if $(hadoop fs -test -d /output) ;
 then
@@ -12,4 +33,5 @@ else
   echo "/output directory not found."; 
 fi
 
-hadoop jar SentimentAnalysis.jar SentimentAnalysis /input /output /tmp/emotionCategory.txt > log.txt 2>&1 
+
+hadoop jar SentimentAnalysis.jar SentimentAnalysis /input /output /tmp/emotionCategory.txt 1>>stdout.txt 2>>stderr.txt
